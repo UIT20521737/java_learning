bb77bced5a6e3f3d99bd5e92f43932650dd1793e
UIT20521737 <20521737@gm.uit.edu.vn>
2025-01-28 13:22:29 +0700

    them vai bai tap chuong 1

---
+++ BaiTap/Chuong1/Bai2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Bai2 {
+
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    double PI = 3.14f;
+    System.out.print("Ente radius: ");
+    double r = myObj.nextDouble();
+    System.out.printf("Perimeter is %.3f.\n", 2 * PI * r);
+    System.out.printf("Area is %.3f.\n", PI * r * r);
+  }
+}
---
+++ BaiTap/Chuong1/Bai3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bai3 {
+
+  public long calcutePower(long n) {
+    long p = 1;
+    for (long i = 1; i <= n; i++) {
+      p *= n;
+    }
+    return p;
+  }
+
+  public static void main(String[] args) {
+    Bai3 b = new Bai3();
+    Scanner myObj = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    long n = myObj.nextLong();
+    System.out.printf("Number = %d\n", n);
+    long s = 0;
+    for (long i = 1; i <= n; i++) {
+      s = s + b.calcutePower(i);
+    }
+    System.out.printf("Sum = %d", s);
+  }
+}
